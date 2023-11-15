144cd4b36d3c4b944537dc188b9912801ef5b934
Sonny Young <sonnyy99@hotmail.com>
2023-11-15 12:32:18 -0800

    Create sign in command for cnh

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,20 +1,11 @@
-// ***********************************************************
-// This example support/e2e.ts is processed and
-// loaded automatically before your test files.
-//
-// This is a great place to put global configuration and
-// behavior that modifies Cypress.
-//
-// You can change the location of this file or turn off
-// automatically serving support files with the
-// 'supportFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/configuration
-// ***********************************************************
+/* eslint-disable @typescript-eslint/no-namespace */
+export {};
+declare global {
+    namespace Cypress {
+        interface Chainable {
+            cnhLogin(username: string, password: string): Chainable<void>;
+        }
+    }
+}
 
-// Import commands.js using ES2015 syntax:
-import './commands'
-
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
+require("./commands")
