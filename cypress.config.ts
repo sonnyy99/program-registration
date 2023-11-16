e0c1cc4f15c03bed5a6a564775ddc0f8cfb92586
Sonny Young <sonnyy99@hotmail.com>
2023-11-15 16:40:14 -0800

    Add time checker to wait for program registration to open

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,13 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents() {
-      // implement node event listeners here
+    setupNodeEvents(on) {
+      on("task", {
+        getCurrentMinute() {
+          const date = new Date();
+          return date.getUTCMinutes();
+        },
+      });
     },
     baseUrl: "https://regportal.cnh.bc.ca",
   },
