c87aaf393377376eabc0c27b7e4d69252c6bcfd6
Sonny Young <sonnyy99@hotmail.com>
2023-11-15 18:44:18 -0800

    fix time waiting task to include hour check

--- cypress/e2e/cnhVolleyballRegistration.cy.ts
+++ cypress/e2e/cnhVolleyballRegistration.cy.ts
@@ -1,13 +1,18 @@
 describe("Program Registration", () => {
   const registrationMinute = 30;
+  const registrationHour = 4; // 8PM PST is 4AM in UTC
   const oneDayInMs = 86400000;
   specify("Sign in and register", () => {
     cy.cnhLogin(Cypress.env("username"), Cypress.env("password"));
     cy.waitUntil(
       () =>
         cy
-          .task("getCurrentMinute")
-          .then((minute) => minute === registrationMinute),
+          .task("getCurrentTime")
+          .then(
+            (currentTime) =>
+              currentTime["minute"] === registrationMinute &&
+              currentTime["hour"] === registrationHour
+          ),
       { timeout: oneDayInMs, interval: 1000 } // 1 day timeout, check again every second
     ).then(() => {
       cy.cnhLogin(Cypress.env("username"), Cypress.env("password")); // Will be skipped if the previous login session has not expired
