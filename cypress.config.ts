e79a69576fee221809e731cab231937f31ea0155
Sonny Young <sonnyy99@hotmail.com>
2023-11-28 22:38:16 -0800

    increase viewport size

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   video: true,
+  viewportHeight: 1080,
+  viewportWidth: 1920,
   defaultCommandTimeout: 15000,
   e2e: {
     setupNodeEvents(on) {
